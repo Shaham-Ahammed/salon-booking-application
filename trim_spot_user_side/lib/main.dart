a0e97acb6f16b0198bea182eca85c4bd28ed2c59
shaham ahammed p k <shahamahammed66@gmail.com>
2024-04-13 21:05:27 +0530

    integrated firebase

--- trim_spot_user_side/lib/main.dart
+++ trim_spot_user_side/lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trim_spot_user_side/firebase_options.dart';
 import 'package:trim_spot_user_side/utils/colors.dart';
 
 import 'blocs/on boarding bloc/onboardind_bloc_bloc.dart';
 import 'screens/introduction_page.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider<OnboardindBloc>(create: (context) => OnboardindBloc())
