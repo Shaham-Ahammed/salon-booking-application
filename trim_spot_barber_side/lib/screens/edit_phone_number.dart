97c100537dc5f754a03731728ddba22b1e5d2c1a
shaham ahammed p k <shahamahammed66@gmail.com>
2024-04-13 10:39:36 +0530

    structured code of profile section

--- trim_spot_barber_side/lib/screens/edit_phone_number.dart
+++ trim_spot_barber_side/lib/screens/edit_phone_number.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:trim_spot_barber_side/screens/otp_verification_profile_section.dart';
 import 'package:trim_spot_barber_side/utils/colors.dart';
-import 'package:trim_spot_barber_side/utils/font.dart';
 import 'package:trim_spot_barber_side/utils/mediaquery.dart';
-import 'package:trim_spot_barber_side/utils/page_transitions/no_transition_page_route.dart';
-import 'package:trim_spot_barber_side/utils/profile_screen/controllers.dart';
-import 'package:trim_spot_barber_side/utils/submit_buttons.dart';
+import 'package:trim_spot_barber_side/widgets/profile_edit_phone_widgets/appbar.dart';
+import 'package:trim_spot_barber_side/widgets/profile_edit_phone_widgets/captions.dart';
+import 'package:trim_spot_barber_side/widgets/profile_edit_phone_widgets/textfield.dart';
+import 'package:trim_spot_barber_side/widgets/profile_edit_phone_widgets/verify_button.dart';
 
 class EditPhoneScreen extends StatelessWidget {
   const EditPhoneScreen({super.key});
@@ -14,24 +13,7 @@ class EditPhoneScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: blackColor,
-      appBar: AppBar(
-        backgroundColor: appBarColor,
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: Icon(
-              Icons.arrow_back_ios_new_rounded,
-              size: mediaqueryHeight(0.03, context),
-              color: whiteColor,
-            )),
-        title: myFont("Edit Phone",
-            fontFamily: belleza,
-            fontSize: mediaqueryHeight(0.03, context),
-            fontWeight: FontWeight.w500,
-            fontColor: whiteColor),
-      ),
+      appBar: appBarEditPhone(context),
       body: SafeArea(
           child: Padding(
         padding: EdgeInsets.all(mediaqueryHeight(0.02, context)),
@@ -39,51 +21,21 @@ class EditPhoneScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              myFont("This mobile number will be verified by an OTP",
-                  fontFamily: balooChettan,
-                  fontSize: mediaqueryHeight(0.015, context),
-                  fontWeight: FontWeight.normal,
-                  fontColor: greyColor),
+              caption(context),
               SizedBox(
                 height: mediaqueryHeight(0.016, context),
               ),
-              Container(
-                padding: EdgeInsets.symmetric(
-                    horizontal: mediaqueryWidth(0.02, context)),
-                decoration: BoxDecoration(
-                    color: greyColor, borderRadius: BorderRadius.circular(8)),
-                width: double.infinity,
-                height: mediaqueryHeight(0.06, context),
-                child: TextFormField(
-                  controller: newPhoneController,
-                  cursorColor: blackColor,
-                  maxLength: 10,
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
-                      hintStyle: TextStyle(fontSize: 14),
-                      hintText: "enter new number",
-                      counterText: "",
-                      focusedBorder: InputBorder.none,
-                      border: InputBorder.none),
-                ),
-              ),
+              Textfield(),
               SizedBox(
                 height: mediaqueryHeight(0.65, context),
               ),
-              Center(
-                  child: submitButtonCyan(context,
-                      heigh: mediaqueryHeight(0.05, context),
-                      text: "verify",
-                      fontSize: mediaqueryHeight(0.024, context),
-                      width: mediaqueryWidth(0.35, context), function: () {
-                Navigator.of(context).push(NoTransitionPageRoute(
-                    child: OtpVerficationProfileSection()));
-                return null;
-              }))
+              VerifyButton()
             ],
           ),
         ),
       )),
     );
   }
+
 }
+
