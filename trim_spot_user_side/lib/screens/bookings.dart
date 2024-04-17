c52951d21f0cea94eb2a2566a51d0ca01fe942cf
shaham ahammed p k <shahamahammed66@gmail.com>
2024-04-17 21:38:37 +0530

    my bookings completed

--- trim_spot_user_side/lib/screens/bookings.dart
+++ trim_spot_user_side/lib/screens/bookings.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:trim_spot_user_side/utils/colors.dart';
+import 'package:trim_spot_user_side/utils/mediaquery.dart';
+import 'package:trim_spot_user_side/utils/screen_padding.dart';
+import 'package:trim_spot_user_side/widgets/bookings/heading.dart';
+import 'package:trim_spot_user_side/widgets/bookings/listview.dart';
 
 class BookingsScreen extends StatelessWidget {
   const BookingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("bookings")),
+    return Scaffold(
+      backgroundColor: blackColor,
+      body: SafeArea(
+        child: Padding(
+          padding: commonScreenPadding(context),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              bookingsScreenHeading(context),
+              SizedBox(
+                height: mediaqueryHeight(0.025, context),
+              ),
+              Expanded(child: bookingsListViewDisplay())
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
